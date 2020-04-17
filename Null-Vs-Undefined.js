263defe90f4cb5c2d942c6300a2e38dc6d050377
masumbillha05 <maisaraislam19@gmail.com>
2020-04-17 15:34:02 +0600

    For a clear interview Null-Vs-Undefined

---
+++ Null-Vs-Undefined.js
@@ -0,0 +1,21 @@
+// let Sojib;
+// console.log(Sojib);
+
+// function add (num1, num2){
+//     console.log(num1 + num2);
+// }
+// const result = add (15, 15);
+// console.log(result);
+
+// const bio = { name: "sojib", phone: 875053};
+// console.log(bio.id);
+
+// let moja = undefined;
+// console.log(moja);
+
+// let age = [54, 76, 87, 56];
+// console.log(age[78]);
+
+
+
+
