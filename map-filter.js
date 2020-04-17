693f5c6290965605da7f1684830ea4ae2ee934ff
masumbillha05 <maisaraislam19@gmail.com>
2020-04-17 16:18:28 +0600

    map, filter, find, smart way to run for loop

---
+++ map-filter.js
@@ -0,0 +1,7 @@
+const numbers = [3, 7, 9, 5, 3, 7, 9];
+const output = [];
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers [i];
+    output.push(result);
+}
+console.log(output);
