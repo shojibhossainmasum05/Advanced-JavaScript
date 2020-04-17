8221901510aa709e9dc61d54a59202d04e7031fd
masumbillha05 <maisaraislam19@gmail.com>
2020-04-17 15:06:06 +0600

    True and False Tips

---
+++ Truthy-Falsy.js
@@ -0,0 +1,18 @@
+// const name = 0 ;
+// const name = "0";
+// const name = NaN;
+// const name = " ";
+// const name = null;
+// const name = {};
+// const name= false;
+// const name = [];
+// const name = undefined;
+// const name = true;
+if (name) {
+    console.log("Condition is true");
+    
+}
+else{
+    console.log("Condition is falsy");
+    
+}
